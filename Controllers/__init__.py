4aee90301350258cb98ab185cef7069fb5bd08d1
xslyr <wesleysilv@gmail.com>
2024-03-13 16:23:59 -0300

    commit inicial

---
+++ Controllers/__init__.py
@@ -0,0 +1,7 @@
+
+
+class ObjetosNegocio(Enum):
+	Produto = 1
+	Cliente = 2
+	Fornecedor = 3
+
---
+++ main.py
@@ -0,0 +1,12 @@
+#/usr/bin/python3
+#!encoding:utf8
+
+
+
+
+
+
+
+if __name__=="__main__":
+	Instance()
+	pass
